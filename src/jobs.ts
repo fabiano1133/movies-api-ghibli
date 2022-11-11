21fcd7c000f6604fdccda2b13ef583b3d7b43eca
fabiano1133 <fabiano.santos0@icloud.com>
2022-11-11 12:48:23 -0300

    feat: implement jobs

--- src/jobs.ts
+++ src/jobs.ts
@@ -2,5 +2,15 @@ import { api } from './config/api/api';
 
 export const getFilms = async () => {
     const response = await api.get('/films');
-    console.log(response.data);
+    console.log(
+        response.data.map((film: any) => {
+            return {
+                titulo: film.title,
+                titulo_original: film.original_title,
+                descricao: film.description,
+                data_lancamento: film.release_date,
+                pontuacao: film.rt_score,
+            };
+        })
+    );
 };
